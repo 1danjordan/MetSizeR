a519234c4e56dcf8643b016ba1ff63dbfa6985ac
Dan Jordan <dandermotj@gmail.com>
2016-06-06 14:41:44 +0100

    began sim_pilot_data

---
+++ R/sim_pilot_data.R
@@ -0,0 +1,6 @@
+#' Simulating data using pilot data
+#'
+#' \code{sim_pilot_data} simulates data using pilot data provided by the user
+#'
+
+sim_pilot_data <- function
--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,20 @@
 #' Scale atomic vector to lie between 0 and 1
 #'
-#' This function takes an atomic vector of data and scales it to
+#' This function takes a vector of data and scales it to
 #' lie between the values 0 and 1. It is intended as a helper function
 #' and can be used with \code{map}
 #'
-#' @param x An atomic vector
+#' @param x A vector
+#'
 #' @examples
+#' # Standardize a vector
 #' x <- rnorm(100, 3, 5)
 #' standardize(x)
 #'
+#' # Standardize the columns of a data frame
 #' iris %>%
 #'  select(-Species) %>%
-#'  dmap(standardize)
+#'  purrr::dmap(standardize)
 
 standardize <- function(x) {
   .min <- min(x)
@@ -19,3 +22,5 @@ standardize <- function(x) {
 
   (x - .min) / (.max - .min)
 }
+
+#' Sample from the null distribution
