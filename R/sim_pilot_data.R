51b0edacf4135d3f03729298f6c4a0c585b467b5
Dan Jordan <dandermotj@gmail.com>
2016-06-07 12:10:47 +0100

    sim_PPCCA_data_with_pilot function written

--- R/sim_pilot_data.R
+++ R/sim_pilot_data.R
@@ -39,12 +39,53 @@ sim_pilot_data <- function
  # I think there might be a mistake in here, given this is supposed to be PPCA but
  # the PPCCA coefficients end up in there...?
 
- sim_PPCA_data_with_pilot <- function(n_samps, n_pilot_vars, n_covars, n_latent_dims, ppcca_coefs){
+ sim_PPCA_data_with_pilot <- function(n_samps,
+                                      n_pilot_vars,
+                                      n_covars,
+                                      n_latent_dims,
+                                      ppcca_coefs) {
 
    C <- rmvnorm(n, rep(0, n_covars), diag(n_covars)) %>%
      map(standardize) %>%
      rbind(rep(1, n), t(.))
 
-   u <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims)) + t(ppcca_coefs %*% C)
+   u <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims)) +
+     t(ppcca_coefs %*% C)
 
  }
+
+ #' Simulate data for PPCCA when pilot data is present
+ #'
+ #'@param n                  number of samples
+ #'@param n_latent_dims      number of latent dimensions
+ #'@param ppca_sigma         the posterior mode estimate of the variance
+ #'                          of the error terms (from the MetabolAnalyze docs)
+ #'@param ppca_loadings      a loadings matrix
+ #'@param pilot_var_means    the means of the variables from the pilot data
+
+ sim_PPCCA_data_with_pilot <- function(n, n_pilot_vars,   # p
+                                       n_latent_dims,     # q
+                                       ppca_sigma,        # sig
+                                       ppca_loadings,     # W
+                                       pilot_var_means) {  # mu
+
+   u <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims))
+
+   x <- rmvnorm(n, rep(0, n_pilot_vars), ppca_sigma * diag(n_pilot_vars)) +
+     tcrossprod(u, ppca_loadings) +
+     matrix(pilot_var_means, n, n_pilot_vars, byrow=TRUE)
+
+   return(x)
+ }
+
+
+
+
+
+
+
+
+
+
+
+
