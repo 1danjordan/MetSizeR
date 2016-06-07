f12ec4aee23dce653e97420d43b98dc947e78a0e
Dan Jordan <dandermotj@gmail.com>
2016-06-07 14:49:06 +0100

    sim_DPPCA_data_with_pilot midway through refactor

--- R/sim_pilot_data.R
+++ R/sim_pilot_data.R
@@ -1,33 +1,3 @@
-#' Simulating data using pilot data
-#'
-#' \code{sim_pilot_data} simulates data using pilot data provided by the user
-#'
-
-sim_pilot_data <- function
-
-
-#' Simulating data using pilot data with the PPCA model
-#'
-#'
-#'
-#' 1 = PPCA; 2 = PPCCA; 3 = DPPCA
-
-# PPCA
-   C <- rmvnorm(n, ZeroL, IL)
-   # Standardize covariates for stability
-   C <- standardize(C)
-   C <- rbind(rep(1, n), t(C))
-   u <- rmvnorm(n, Zeroq, Iq) + t(Alpha %*% C)
-   }#ifppca
-
- x <- rmvnorm(n, Zerop, sig * Ip) +
-   tcrossprod(u, W) +
-   matrix(mu, n, p, byrow=TRUE)
-
-# If PPCCA
- u <- rmvnorm(n,Zeroq,Iq)
- x <- rmvnorm(n, Zerop, sig * Ip) + tcrossprod(u, W) + matrix(mu, n, p, byrow=TRUE)
-
  #' Simulate data for PPCA when pilot data is present
  #'
  #' @param n_samps number of samples
@@ -35,9 +5,10 @@ sim_pilot_data <- function
  #' @param n_covars the number of covariates
  #' @param n_latent_dims the number of latent dimensions (set as 2 always)
 
- # This name of the function along with the argument names are obviously changing
- # I think there might be a mistake in here, given this is supposed to be PPCA but
- # the PPCCA coefficients end up in there...?
+ # This name of the function along with the argument names are
+ # obviously changing  I think there might be a mistake in here,
+ # given this is supposed to be PPCA but the PPCCA coefficients
+ # end up in there...?
 
  sim_PPCA_data_with_pilot <- function(n_samps,
                                       n_pilot_vars,
@@ -45,12 +16,14 @@ sim_pilot_data <- function
                                       n_latent_dims,
                                       ppcca_coefs) {
 
-   C <- rmvnorm(n, rep(0, n_covars), diag(n_covars)) %>%
+   u <- rmvnorm(n, rep(0, n_covars), diag(n_covars)) %>%
      map(standardize) %>%
      rbind(rep(1, n), t(.))
 
-   u <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims)) +
-     t(ppcca_coefs %*% C)
+   x <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims)) +
+     t(ppcca_coefs %*% u)
+
+   return(x)
 
  }
 
@@ -59,15 +32,19 @@ sim_pilot_data <- function
  #'@param n                  number of samples
  #'@param n_latent_dims      number of latent dimensions
  #'@param ppca_sigma         the posterior mode estimate of the variance
- #'                          of the error terms (from the MetabolAnalyze docs)
+ #'                          of the error terms (MetabolAnalyze docs)
  #'@param ppca_loadings      a loadings matrix
  #'@param pilot_var_means    the means of the variables from the pilot data
+
+
+ # I think I might pass the whole MetabolAnalyze list object
+ # into this function
 
  sim_PPCCA_data_with_pilot <- function(n, n_pilot_vars,   # p
                                        n_latent_dims,     # q
                                        ppca_sigma,        # sig
                                        ppca_loadings,     # W
-                                       pilot_var_means) {  # mu
+                                       pilot_var_means) { # mu
 
    u <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims))
 
@@ -78,8 +55,41 @@ sim_pilot_data <- function
    return(x)
  }
 
+# The variable names are a total mess and I don't know what
+# any of them refer to or their meaning really so I'm just trying
+# to back track them and simplify from there
+
+sim_DPPCA_data_with_pilot <- function(){
+
+  v              <- 0.1                 # v2.true
+  phi            <- 0.8                 # phi.true
+  alpha_sigma    <- 5                   # ao | alpha.sigma <- ao <- 5
+                                        # the scale parameter of the prior
+                                        # distribution of the variance.
+  mu <- rep(0, n_latent_dims)           # zero vector for rmvnorm mean arg
+
+  # SV model on the errors
+  eta_sd   <- sqrt(v / (1 - phi^2))                 # eta.sd
+  # a square matrix with values only on the diagonal
+  eta_sc_sd <- eta_sd %>% diag(n_latent_dims)
 
 
+  u_sigma <- rmvnorm(1, mu, eta_sc_sd) %>% diag(n_latent_dims)
+  u <- rmvnorm(n, mu, diag(eta_sc_true, n_latent_dims))
+
+  # TODO: There's definitely something fishy with passing
+  # the vector b into rgamma().
+
+  b <- c(0.5 * (alpha_sigma - 1), 0.25 * (alpha_sigma - 1)) # bo
+  w_sigma <- 1 / rgamma(n_latent_dims, alpha_sigma, b) %>% diag(n_latent_dims)
+  w <- rmvnorm(n_pilot_vars, mu, w_sigma)
+
+
+  x_sigma <- rnorm(1, 0, eta_sd) %>% exp() %>% diag(n_latent_dims)
+  x <- rmvnorm(n, mu, x_sigma) + tcrossprod(u, w)
+
+  return(x)
+}
 
 
 
