ca0d3fbb13f5bf0a2dea09b3901bfce48a5ca098
Dan Jordan <dandermotj@gmail.com>
2016-06-08 21:57:19 +0100

    sim_PPCCA_data_with_pilot working

--- R/sim_pilot_data.R
+++ R/sim_pilot_data.R
@@ -1,10 +1,7 @@
 #' Simulate data for PPCCA when pilot data is present
 #'
 #'@param n                  number of samples
-#'@param n_latent_dims      number of latent dimensions
-#'@param ppca_sigma         the posterior mode estimate of the variance
-#'                          of the error terms (MetabolAnalyze docs)
-#'@param ppca_loadings      a loadings matrix
+#'@param ppca_obj           the results from \code{ppca.metabol}
 #'@param pilot_var_means    the means of the variables from the pilot data
 
 sim_PPCA_data_with_pilot <- function(n, ppca_obj, pilot_var_means) {
@@ -14,7 +11,7 @@ sim_PPCA_data_with_pilot <- function(n, ppca_obj, pilot_var_means) {
 
   u <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims))
 
-  x <- rmvnorm(n, rep(0, n_pilot_vars), ppca_obj$sig * diag(n_pilot_vars)) +
+  x <- rmvnorm(n, rep(0, n_pilot_vars), ppca_obj$sig*diag(n_pilot_vars)) +
     tcrossprod(u, ppca_obj$loadings) +
     matrix(pilot_var_means, n, n_pilot_vars, byrow=TRUE)
 
@@ -22,34 +19,36 @@ sim_PPCA_data_with_pilot <- function(n, ppca_obj, pilot_var_means) {
 }
 
 #' Simulate data for PPCA when pilot data is present
- #'
- #' @param n_samps number of samples
- #' @param n_pilot_vars the number of variables in the pilot data
- #' @param n_covars the number of covariates
- #' @param n_latent_dims the number of latent dimensions (set as 2 always)
+#'
+#' @param n_samps number of samples
+#' @param n_pilot_vars the number of variables in the pilot data
+#' @param n_covars the number of covariates
+#' @param n_latent_dims the number of latent dimensions (set as 2 always)
 
- # This name of the function along with the argument names are  changing
+# Sampling 1 obs produces NaNs
+# returns matrix with column names
 
- sim_PPCCA_data_with_pilot <- function(n_samps,
-                                      n_pilot_vars,
-                                      n_covars,
-                                      n_latent_dims,
-                                      ppca_obj) {
+sim_PPCCA_data_with_pilot <- function(n, ppcca_obj, pilot_var_means) {
 
-   u <- rmvnorm(n, rep(0, n_covars), diag(n_covars)) %>%
-     map(standardize) %>%
-     rbind(rep(1, n), t(.))
+  n_pilot_vars  <- nrow(ppcca_obj$loadings)
+  n_latent_dims <- ncol(ppcca_obj$loadings)
+  n_covars      <- ncol(ppcca_obj$coefficients) - 1
 
-   u <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims)) +
-     t(ppca_obj$coefs %*% u)
+  c <- rmvnorm(n, rep(0, n_covars), diag(n_covars)) %>%
+    apply(2, standardize) %>%
+    cbind(1, .)
 
-   x <- rmvnorm(n, rep(0, n_pilot_vars), ppca_sigma * diag(n_pilot_vars)) +
-     tcrossprod(u, ppca_loadings) +
-     matrix(pilot_var_means, n, n_pilot_vars, byrow=TRUE)
+  u <- rmvnorm(n, rep(0, n_latent_dims), diag(n_latent_dims)) +
+    t(tcrossprod(ppcca_obj$coefficients, c))
 
-   return(x)
+  x <- rmvnorm(n, rep(0, n_pilot_vars), diag(ppcca_obj$sig, n_pilot_vars)) +
+    tcrossprod(u, ppcca_obj$loadings) +
+    matrix(pilot_var_means, n, n_pilot_vars, byrow = TRUE, dimnames = NULL)
+
+  return(x)
 
  }
+
 
 
 
