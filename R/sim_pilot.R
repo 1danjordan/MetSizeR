f2cd5d9fcb0f439f8bc1e83a757ee1a7613edda7
Dan Jordan <dandermotj@gmail.com>
2016-06-14 00:02:31 +0100

    output tests for simulations

--- R/sim_pilot.R
+++ R/sim_pilot.R
@@ -3,6 +3,10 @@
 #'
 #' @param n                 the number of samples to be simulated
 #' @param n_spectral_bins   the number of spectral bins considered
+#'
+#' @example
+#' # Simulate 100 samples with 40 spectral bins
+#' sim_PPCA(100, 40)
 
 sim_PPCA <- function(n, n_spectral_bins) {
 
@@ -17,7 +21,7 @@ sim_PPCA <- function(n, n_spectral_bins) {
   sig <- 1 / rgamma(1, alpha.sigma, beta.sigma)
 
   u <- rmvnorm(n, rep(0, n_latent_vars), diag(n_latent_vars))
-  v <- 1 / rgamma(n_latent_vars, ao, bo)
+  v <- 1 / rgamma(n_latent_vars, alpha.sigma, bo)
   w <- rmvnorm(n_spectral_bins, rep(0, n_latent_vars), diag(v))
 
   x <- rmvnorm(n, rep(0, n_spectral_bins), diag(sig, n_spectral_bins))
@@ -39,16 +43,17 @@ sim_PPCCA <- function(n, n_spectral_bins, n_covars) {
   alpha.sigma   <- 5
   # the shape parameter of the prior distribution of the variance
   beta.sigma    <- 2 * (alpha.sigma - 1)
+  bo <- c(0.5 * (alpha.sigma - 1), 0.25 * (alpha.sigma - 1))
   sig <- 1 / rgamma(1,alpha.sigma,beta.sigma)
 
-  Alpha <- rmvnorm(n_latent_vars, rep(0, ncovars + 1), diag(3 * (n_covars + 1)))
+  Alpha <- rmvnorm(n_latent_vars, rep(0, n_covars + 1), diag(3, (n_covars + 1)))
 
-  c <- rmvnorm(n,rep(0, ncovars), diag(n_covars)) %>%
+  c <- rmvnorm(n,rep(0, n_covars), diag(n_covars)) %>%
     apply(2, standardize) %>%
     cbind(1, .)
 
-  u <- rmvnorm(n, rep(0, n_latent_vars), rep(n_latent_vars)) + t(tcrossprod(Alpha, c))
-  v <- 1 / rgamma(n_latent_vars, ao, bo)
+  u <- rmvnorm(n, rep(0, n_latent_vars), diag(n_latent_vars)) + t(tcrossprod(Alpha, c))
+  v <- 1 / rgamma(n_latent_vars, alpha.sigma, bo)
   w <- rmvnorm(n_spectral_bins, rep(0, n_latent_vars), diag(v))
 
   x <- rmvnorm(n, rep(0, n_spectral_bins), diag(sig, n_spectral_bins))
@@ -71,7 +76,7 @@ sim_DPPCA <- function(n, n_pilot_vars) {
 
   # the scale parameter of the prior distribution of the variance.
   alpha.sigma <- 5
-  b <- c(0.5 * (alpha_sigma - 1), 0.25 * (alpha_sigma - 1))
+  b <- c(0.5 * (alpha.sigma - 1), 0.25 * (alpha.sigma - 1))
 
   rmvnorm_fun <- partial(rmvnorm, method = "svd")
 
@@ -79,7 +84,7 @@ sim_DPPCA <- function(n, n_pilot_vars) {
     diag(n_latent_dims) %>%
     rmvnorm_fun(n, rep(0, n_latent_dims), .)
 
-  w <- (1 / rgamma(n_latent_dims, alpha_sigma, b)) %>%
+  w <- (1 / rgamma(n_latent_dims, alpha.sigma, b)) %>%
     diag(n_latent_dims) %>%
     rmvnorm_fun(n_pilot_vars, rep(0, n_latent_dims), .)
 
--- tests/testthat/test-simulation-funs.R
+++ tests/testthat/test-simulation-funs.R
@@ -1,21 +1,17 @@
 context("Test simulation functions work")
 
-library(MetabolAnalyze, quietly = TRUE)
-data(UrineSpectra)
+library(mvtnorm)
+library(purrr)
+
+data(UrineSpectra, package = "MetabolAnalyze")
 pilot_data <- UrineSpectra[[1]]
 covariates <- UrineSpectra[[2]][,2]
-n <- 100
-
-# I want to pull all of these out into a function because
-# they are all the same tests but I'm not sure how
-# to do it properly
-
-
 
 test_that("sim_PPCA_pilot outputs correct dimensions and type", {
 
-  ppca_fit <- ppca.metabol(pilot_data)
+  ppca_fit <- MetabolAnalyze::ppca.metabol(pilot_data, printout = FALSE)
 
+  n <- 100
   sims <- sim_PPCA_pilot(n, ppca_fit, colMeans(pilot_data))
 
   expect_equal(nrow(sims), n)
@@ -25,8 +21,9 @@ test_that("sim_PPCA_pilot outputs correct dimensions and type", {
 
 test_that("sim_PPCCA_pilot outputs correct dimensions and type", {
 
-  ppcca_fit <- ppcca.metabol(pilot_data, covariates)
+  ppcca_fit <- MetabolAnalyze::ppcca.metabol(pilot_data, covariates, printout = FALSE)
 
+  n <- 100
   sims <- sim_PPCCA_pilot(n, ppcca_fit, colMeans(pilot_data))
 
   expect_equal(nrow(sims), n)
@@ -36,11 +33,44 @@ test_that("sim_PPCCA_pilot outputs correct dimensions and type", {
 
 test_that("sim_DPPCA_pilot outputs correct dimensions and type", {
 
-  ppca_fit <- ppca.metabol(pilot_data)
+  ppca_fit <- MetabolAnalyze::ppca.metabol(pilot_data, printout = FALSE)
 
+  n <- 100
   sims <- sim_DPPCA_pilot(n, ppca_fit)
 
   expect_equal(nrow(sims), n)
   expect_equal(ncol(sims), ncol(pilot_data))
+  expect_is(sims, "matrix")
+})
+
+test_that("sim_PPCA outputs correct dimension and type", {
+
+  n <- 100
+  n_spectral_bins <- 40
+  sims <- sim_PPCA(n, n_spectral_bins)
+
+  expect_equal(nrow(sims), n)
+  expect_equal(ncol(sims), n_spectral_bins)
+  expect_is(sims, "matrix")
+})
+
+test_that("sim_PPCCA outputs correct dimension and type", {
+  n <- 100
+  n_spectral_bins <- 40
+  n_covars <- 3
+  sims <- sim_PPCCA(n, n_spectral_bins, n_covars)
+
+  expect_equal(nrow(sims), n)
+  expect_equal(ncol(sims), n_spectral_bins)
+  expect_is(sims, "matrix")
+})
+
+test_that("sim_DPPCA outputs correct dimension and type", {
+  n <- 100
+  n_spectral_bins <- 40
+  sims <- sim_DPPCA(n, n_spectral_bins)
+
+  expect_equal(nrow(sims), n)
+  expect_equal(ncol(sims), n_spectral_bins)
   expect_is(sims, "matrix")
 })
