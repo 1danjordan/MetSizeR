ebea7c8b98ddac0274a29518c9d3af92243b6b3e
Dan Jordan <dandermotj@gmail.com>
2016-06-22 15:08:49 +0100

    get_model_fun

--- R/metsize.R
+++ R/metsize.R
@@ -1,3 +1,24 @@
+#' Select model to generate simulations with
+#'
+#'
+
+get_model_fun <- function(model, pilot = NULL) {
+
+  if(is_null(pilot)) {
+    switch(model,
+           ppca  = sim_PPCA,
+           ppcca = sim_PPCCA,
+           dppca = sim_DPPCA)
+  } else {
+    switch(model,
+           ppca  = sim_PPCA_pilot,
+           ppcca = sim_PPCCA_pilot,
+           dppca = sim_DPPCA_pilot)
+  }
+
+}
+
+
 #' Determine the sample size at which the FDR line is equal to 0.05
 #'
 #'
@@ -10,6 +31,9 @@
 #  3 - 90th percentile FDR of resampled simulations
 #  4 - 10th percentile FDR of resampled simulations
 
+# I genuinely feel like this is incorrect. It just isn't making
+# sense
+
 est_sample_size <- function(results_sim, target.fdr) {
 
   # index 1
@@ -17,17 +41,27 @@ est_sample_size <- function(results_sim, target.fdr) {
   #  1 take all the rows
   #  2 find the rows where the median FDR is less than 0.05
   #  3 get them minimum row number
-  # I feel like this should be sorted first...
+  #  why get the minimum row number?...
+
 
   ind1 <- min(c(1:nrow(results_sim))[results_sim[, 2] < 0.05])
-  # index 2
-  ind2 <- max(c(1:ind1)[results_sim[1:ind1,2]>0.05])
+  # 1 ... ind1
 
+  # index 2
+  # this is the index where:
+  #   we take the result where the media FDR is greater than 0.05
+  #   then we find the index of all of them
+  #   the we get the highest index
+
+  ind2 <- max(c(1:ind1)[results_sim[1:ind1, 2] > 0.05])
+  # 1 ... ind2
+
+  # is this supposed to be some form of linear interpolation?
   opty <- c(results_sim[ind1,2], results_sim[ind2,2])
   optx <- c(results_sim[ind1,1], results_sim[ind2,1])
 
   # linear model of opty on optx
-  optres <- lm(opty~optx)
+  optres <- lm(opty ~ optx)
 
   # estimated sample size
   nhat <- round((target.fdr - optres$coef[1])/optres$coef[2])
